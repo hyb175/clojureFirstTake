ae76174f7e95f03084fd474356f2d33be1622995
Yingbai He <yingbai.he93@gmail.com>
2015-10-29 03:42:46 -0700

    add global index

--- src/brainfuck/core.clj
+++ src/brainfuck/core.clj
@@ -1,13 +1,33 @@
 (ns brainfuck.core
   (:require [clojure.java.io :as io]))
-(defn processLine [brainfuckArray index line]
+
+(def arrayIndex (atom 0))
+
+(defn get-arrayIndex []
+  @arrayIndex)
+
+(defn inc-arrayIndex []
+  (reset! arrayIndex (inc @arrayIndex)))
+
+(defn processLine [brainfuckArray arrayIndex line]
   (doseq [c line]
+    (case c
+      ">" (+ arrayIndex 1)
+      "<" (println "arrayIndex")
+      "+"
+      "-"
+      "."
+      ","
+      "["
+      "]"
+      "default"
+      )
     (println c))
   )
 
 (defn brainfuckLOL []
   (def brainfuckArray (byte-array 30000))
-  (def index 0)
+  (println arrayIndex)
   (with-open [rdr (io/reader "/Users/yingbaihe/brainfuck")]
   (doseq [line (line-seq rdr)]
-  (processLine brainfuckArray index line))))
+  (processLine brainfuckArray arrayIndex line))))
