12fefc5fd0d0d6e92140fb74f7157cf9929cf292
Yingbai He <nicky.he.yb@gmail.com>
2015-10-29 13:20:57 -0700

    wow almost getting it to work

--- src/brainfuck/core.clj
+++ src/brainfuck/core.clj
@@ -9,25 +9,41 @@
 (defn inc-arrayIndex []
   (reset! arrayIndex (inc @arrayIndex)))
 
-(defn processLine [brainfuckArray arrayIndex line]
-  (doseq [c line]
-    (case c
-      ">" (+ arrayIndex 1)
-      "<" (println "arrayIndex")
-      "+"
-      "-"
-      "."
-      ","
-      "["
-      "]"
-      "default"
-      )
-    (println c))
-  )
+(defn dec-arrayIndex []
+  (reset! arrayIndex (dec @arrayIndex)))
 
-(defn brainfuckLOL []
-  (def brainfuckArray (byte-array 30000))
-  (println arrayIndex)
-  (with-open [rdr (io/reader "/Users/yingbaihe/brainfuck")]
-  (doseq [line (line-seq rdr)]
-  (processLine brainfuckArray arrayIndex line))))
+(def brainfuckArray (atom (vec (byte-array 100))))
+
+(defn get-brainfuckArray []
+  @brainfuckArray)
+
+(defn get-brainfuckArray-value []
+  (nth @brainfuckArray (get-arrayIndex)))
+
+(defn update-brainfuckArray [value]
+  (swap! brainfuckArray assoc (get-arrayIndex) value))
+
+
+(defn processLine [line]
+  (let [trackingStack ()]
+    (loop [x 0]
+      (let [lineChars (count line)]
+        (when (< x lineChars)
+          (let [c (nth line x)]
+          (if (= c \])
+            (if (> (get-arrayIndex) 0) (recur (inc (peek trackingStack))) (do (pop trackingStack) (recur (inc x))))
+            (do (case c
+              \> (inc-arrayIndex)
+              \< (dec-arrayIndex)
+              \+ (update-brainfuckArray (inc (get-brainfuckArray-value)))
+              \- (update-brainfuckArray (dec (get-brainfuckArray-value)))
+              \. (println (get-brainfuckArray-value))
+              \, (update-brainfuckArray (.read System/in))
+              \[ (cons trackingStack x)
+              )
+          (recur (inc x))))))))))
+  
+
+(defn brainfuckLOL []  
+  (processLine (slurp (io/reader "/Users/yingbaihe/brainfuck")))
+  (println (get-brainfuckArray)))
