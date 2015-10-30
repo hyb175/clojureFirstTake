bbd3aafb94322de6f521458f84866b0ea0a1a524
Yingbai He <nicky.he.yb@gmail.com>
2015-10-30 01:19:26 -0700

    It is working now

--- src/brainfuck/core.clj
+++ src/brainfuck/core.clj
@@ -1,18 +1,13 @@
 (ns brainfuck.core
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:gen-class))
 
 (def arrayIndex (atom 0))
 
 (defn get-arrayIndex []
   @arrayIndex)
 
-(defn inc-arrayIndex []
-  (reset! arrayIndex (inc @arrayIndex)))
-
-(defn dec-arrayIndex []
-  (reset! arrayIndex (dec @arrayIndex)))
-
-(def brainfuckArray (atom (vec (byte-array 100))))
+(def brainfuckArray (atom (vec (byte-array 1000))))
 
 (defn get-brainfuckArray []
   @brainfuckArray)
@@ -20,30 +15,52 @@
 (defn get-brainfuckArray-value []
   (nth @brainfuckArray (get-arrayIndex)))
 
+(defn inc-arrayIndex []
+  (reset! arrayIndex (inc @arrayIndex)))
+
+(defn dec-arrayIndex []
+  (reset! arrayIndex (dec @arrayIndex)))
+
 (defn update-brainfuckArray [value]
   (swap! brainfuckArray assoc (get-arrayIndex) value))
 
+(defn jump-to [line]
+  (loop [x 0 matchCount 0]
+    (when (and (< x (count line)) (> matchCount -1))
+      (let [c (nth line x)]
+        (case c
+          \[ (recur (inc x) (inc matchCount))
+          \] (if (= matchCount 0) x (recur (inc x) (dec matchCount)))
+          (recur (inc x) matchCount)
+        )
+      )
+    )
+  )
+)
 
 (defn processLine [line]
-  (let [trackingStack ()]
-    (loop [x 0]
-      (let [lineChars (count line)]
-        (when (< x lineChars)
-          (let [c (nth line x)]
-          (if (= c \])
-            (if (> (get-arrayIndex) 0) (recur (inc (peek trackingStack))) (do (pop trackingStack) (recur (inc x))))
+  (loop [x 0 trackingStack ()]
+    (let [lineChars (count line)]
+      (when (< x lineChars)
+        (let [c (nth line x)]
+        (if (= c \])
+          (if (> (get-brainfuckArray-value) 0) (recur (inc (peek trackingStack)) trackingStack) (recur (inc x) (pop trackingStack)))
+          (if (= c \[) (if (> (get-brainfuckArray-value) 0) (recur (inc x) (conj trackingStack x)) (recur (inc (jump-to (subs line (inc x)))) (conj trackingStack x)))
             (do (case c
-              \> (inc-arrayIndex)
-              \< (dec-arrayIndex)
-              \+ (update-brainfuckArray (inc (get-brainfuckArray-value)))
-              \- (update-brainfuckArray (dec (get-brainfuckArray-value)))
-              \. (println (get-brainfuckArray-value))
-              \, (update-brainfuckArray (.read System/in))
-              \[ (cons trackingStack x)
-              )
-          (recur (inc x))))))))))
+            \> (inc-arrayIndex)
+            \< (dec-arrayIndex)
+            \+ (update-brainfuckArray (inc (get-brainfuckArray-value)))
+            \- (update-brainfuckArray (dec (get-brainfuckArray-value)))
+            \. (print (char (get-brainfuckArray-value)))
+            \, (let [ch (.read System/in)] (update-brainfuckArray ch))
+            "default"
+            )
+        (recur (inc x) trackingStack)))))))))
   
 
 (defn brainfuckLOL []  
   (processLine (slurp (io/reader "/Users/yingbaihe/brainfuck")))
   (println (get-brainfuckArray)))
+
+(defn -main [& args]
+  (brainfuckLOL))
